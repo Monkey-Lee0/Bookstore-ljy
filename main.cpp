3820763fd2346aeec9384d810178af3134428a0a
Monkey-Lee0 <2562156161@qq.com>
2024-12-17 10:45:45 +0800

    update cache and database(2)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include<bits/stdc++.h>
 #include"database.h"
 using namespace std;
-B_plus_Tree<name,int> Tree;
 int n;
 string ins;
 name key;
 int value;
+double t,t1;
+struct PP
+{
+    int a[100];
+};
 int main()
 {
     ios::sync_with_stdio(false);
+    // t=clock();
     // freopen("test.in","r",stdin);
     // freopen("test.out","w",stdout);
     ifstream f("tree");
-    Tree.initialize("tree","data",!f.good());
+    B_plus_Tree<name,int> Tree("tree",!f.good());
     f.close();
     cin>>n;
     while(n--)
     {
+        // std::cerr<<n<<std::endl;
         cin>>ins>>key;
         if(ins=="insert")
         {
@@ -25,6 +31,7 @@ int main()
         }
         else if(ins=="find")
         {
+            t1-=clock();
             auto ans=Tree.Find(key);
             if(ans.empty())
                 cout<<"null"<<endl;
@@ -34,6 +41,7 @@ int main()
                     cout<<t<<" ";
                 cout<<endl;
             }
+            t1+=clock();
         }
         else if(ins=="delete")
         {
@@ -41,6 +49,8 @@ int main()
             Tree.Delete(key,value);
         }
     }
+    // t=clock()-t;
+    // std::cerr<<t1/CLOCKS_PER_SEC<<" "<<t/CLOCKS_PER_SEC<<endl;
 
     return 0;
 }
